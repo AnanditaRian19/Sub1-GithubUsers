768872d86176c5c3e18912a638567b1e4abff7a0
AnanditaRian19 <rizkianandita6@gmail.com>
2020-07-08 00:18:22 +0700

    Detail

--- app/src/main/java/com/belajar/githubusers/DetailUsers.java
+++ app/src/main/java/com/belajar/githubusers/DetailUsers.java
@@ -1,15 +1,22 @@
 package com.belajar.githubusers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.belajar.githubusers.model.Users;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
 
@@ -18,6 +25,9 @@ public class DetailUsers extends AppCompatActivity {
     public static final String EXTRA_USERS = "extra_users";
     private TextView tvName, tvUsername, tvFollower, tvFollowing, tvLocation, tvCompany, tvRepository, tvEmail;
     private ImageView ivAvatar;
+    private Button btnFollow;
+    private ToggleButton btnToggle;
+    private boolean isClick = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +46,43 @@ public class DetailUsers extends AppCompatActivity {
         tvCompany = findViewById(R.id.tv_company);
         tvRepository = findViewById(R.id.tv_repository);
         tvEmail = findViewById(R.id.tv_email);
+        btnFollow = findViewById(R.id.btn_follow);
+        btnToggle = findViewById(R.id.btn_toggle);
 
-        Users users = getIntent().getParcelableExtra(EXTRA_USERS);
+        btnToggle.setChecked(false);
+        btnToggle.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_favorite));
+        btnToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//                Users userrr = new Users();
+
+                if (b) {
+                    btnToggle.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_favorite_click));
+//                    Toast toast = Toast.makeText(DetailUsers.this, "Favorite " + userrr.getName(),Toast.LENGTH_SHORT);
+//                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0,0);
+//                    toast.show();
+                } else {
+                    btnToggle.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_baseline_favorite));
+//                    Toast toast = Toast.makeText(DetailUsers.this, "Unfavorite " + userrr.getName(),Toast.LENGTH_SHORT);
+//                    toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL,0,0);
+//                    toast.show();
+                }
+            }
+        });
+
+        btnFollow.setText("Follow");
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    btnFollow.setText("Following");
+
+//                Toast toast = Toast.makeText(DetailUsers.this, "Follow ", Toast.LENGTH_SHORT);
+//                toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+//                toast.show();
+            }
+        });
+
+        final Users users = getIntent().getParcelableExtra(EXTRA_USERS);
 
         Glide.with(DetailUsers.this)
                 .load(users.getAvatar())
